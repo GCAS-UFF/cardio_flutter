71a805096f8fec80d17e9fec004a30c34ddaa9fb
Kelly Bentes <kellybentes@MacBook-Air-de-Barbara.local>
2020-03-11 16:50:31 -0300

    Change manage patient list to manage professional

--- lib/features/manage_professional/domain/repositories/manage_professional_repository.dart
+++ lib/features/manage_professional/domain/repositories/manage_professional_repository.dart
@@ -1,11 +1,14 @@
 import 'package:cardio_flutter/core/error/failure.dart';
 import 'package:cardio_flutter/features/auth/domain/entities/patient.dart';
+import 'package:cardio_flutter/features/auth/domain/entities/professional.dart';
 
 import 'package:dartz/dartz.dart';
 
 abstract class PatientListRepository {
   Future<Either<Failure, List<Patient>>> getPatientList();
-  Future<Either<Failure, Patient>> deletePatientList(Patient patient);
+  Future<Either<Failure, void>> deletePatientList(Patient patient);
   Future<Either<Failure, Patient>> addPatientList(Patient patient);
   Future<Either<Failure, Patient>> editPatientList(Patient patient);
-  }
+  Future<Either<Failure, Professional>> editProfessional(
+      Professional professional);
+}
--- lib/features/manage_professional/domain/usecases/get_patient_list.dart
+++ lib/features/manage_professional/domain/usecases/get_patient_list.dart
@@ -1,14 +1,13 @@
 import 'package:cardio_flutter/core/error/failure.dart';
 import 'package:cardio_flutter/core/usecases/usecase.dart';
 import 'package:cardio_flutter/features/auth/domain/entities/patient.dart';
-
-import 'package:cardio_flutter/features/manage_patients/domain/repositories/manage_patient_repository.dart';
+import 'package:cardio_flutter/features/manage_professional/domain/repositories/manage_professional_repository.dart';
 import 'package:dartz/dartz.dart';
 
 
 
 class GetPatientList extends UseCase<List<Patient>, NoParams> {
-  final PatientListRepository repository;
+  final ManageProfessionalRepository repository;
 
   GetPatientList(this.repository);
 
